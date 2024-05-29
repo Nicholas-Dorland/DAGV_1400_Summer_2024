6e80ce9775faa248b42368409b73ad043bbb6497
Nicholas Dorland <11059821@uvu.edu>
2024-05-29 17:09:57 -0600

    Unit 4 Lab Update
    Prompted for User Input and added comments.

--- Unit 4/NumberPyramid.cs
+++ Unit 4/NumberPyramid.cs
@@ -4,15 +4,22 @@ public class Program
 {
     public static void Main()
     {
-        int i = 1;
-        while (i < 6)
+        /* This Line Prompts the User for a number.*/
+        Console.Write("How tall is your Pyramid? Please type a whole, positive number: ");
+
+        int y = 7;  //Convert.ToInt32(Console.ReadLine());  /*While I cannot test User Input, I'll use 7 for now.*/
+        int i = 1;  /*Starting Point*/
+
+        /*Between 1 and the number input, it will create layers.*/
+        while (i <= y)
         {
-            for (int x = 0; x < i; x++)
+            /*For each value of i, write what i is an i number of times.*/
+            for (int x = 1; x <= i; x++)
             {
                 Console.Write(i);
             }
             Console.WriteLine("");
-            i++;
+            i++;  /*Move to the next line of the Pyramid*/
         }
     }
 }
