06ffe892fcb75bdf9fc4ad479f9802851be6e834
Nicholas Dorland <11059821@uvu.edu>
2024-06-12 16:44:32 -0600

    Unit 6 Progress
    Input and Axis

---
+++ Unit 6/Assets/Axis.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Axis : MonoBehaviour
+{
+    public float range;
+    public Text textOutput;
+
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float xPos = h * range;
+
+        transform.position = new Vector3(xPos, 2f, 0);
+        textOutput.text = "Value Returned: " + h.ToString("F2");
+    }
+}
