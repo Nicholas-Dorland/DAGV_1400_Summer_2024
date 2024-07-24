7dd7dbd4a3103e0e13091825d84fd503c0fdd231
Nicholas Dorland <11059821@uvu.edu>
2024-07-24 12:44:08 -0600

    Unit 12 - Challenge Finish
    Wrap-up of Unit 12's assignments.

--- Unit 12 - Lab/Assets/Scripts/PlayerController.cs
+++ Unit 12 - Lab/Assets/Scripts/PlayerController.cs
@@ -6,14 +6,17 @@ public class PlayerController : MonoBehaviour
 {
     public Transform blaster;
     public GameObject lazerBolt;
+    public GameObject powerupIndicator;
     public float horizontalInput;
     public float speed = 25;
     public float xRange = 30;
+    public bool hasPowerup;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        hasPowerup = false;
+        powerupIndicator.SetActive(false);
     }
 
     // Update is called once per frame
@@ -22,6 +25,8 @@ public class PlayerController : MonoBehaviour
         horizontalInput = Input.GetAxis("Horizontal");
 
         transform.Translate(Vector3.right * horizontalInput * speed * Time.deltaTime);
+
+        powerupIndicator.transform.position = transform.position;
 
         if (transform.position.x < -xRange)
         {
@@ -35,6 +40,30 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             Instantiate(lazerBolt, blaster.transform.position, lazerBolt.transform.rotation);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Shield"))
+        {
+            Destroy(other.gameObject);
+            hasPowerup = true;
+            powerupIndicator.SetActive(true);
+        }
+        else if (other.gameObject.CompareTag("Enemy"))
+        {
+            if (hasPowerup)
+            {
+                hasPowerup = false;
+                powerupIndicator.SetActive(false);
+                Destroy(other.gameObject);
+            }
+            else
+            {
+                Debug.Log("Game Over!");
+                Destroy(gameObject);
+            }
         }
     }
 }
