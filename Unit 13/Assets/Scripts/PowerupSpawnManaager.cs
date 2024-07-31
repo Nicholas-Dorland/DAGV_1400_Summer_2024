0c61c29e7559b11150e37660fff58d2328d1d989
Nicholas Dorland <11059821@uvu.edu>
2024-07-30 21:02:27 -0600

    Unit 13 Update
    Added enemy functionality, varying speed, and projectile collision.

---
+++ Unit 13/Assets/Scripts/PowerupSpawnManaager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerupSpawnManaager : MonoBehaviour
+{
+    public GameObject powerUp;
+
+    private float spawnRangeX = 20.0f;
+    private float spawnPosZ = 25.0f;
+
+    private void Start()
+    {
+        InvokeRepeating("RandomSpawner", 3.0f, 2.0f);
+    }
+
+    void RandomSpawner()
+    {
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 2, spawnPosZ);
+        Instantiate(powerUp, spawnPos, powerUp.transform.rotation);
+    }
+
+}
